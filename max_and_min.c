db3c39b6ce9a981297e7e261d1d9f8f62da30b6c
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-10-16 11:05:39 +0600

    solve even sumber problem

--- max_and_min.c
+++ max_and_min.c
@@ -7,15 +7,15 @@ int main()
 
     if (a <= b && a <= c)
     {
-        printf("%d", a);
+        printf("%d ", a);
     }
     else if (b <= a && b <= c)
     {
-        printf("%d", b);
+        printf("%d ", b);
     }
     else if (c <= a && c <= b)
     {
-        printf("%d", c);
+        printf("%d ", c);
     }
 
     // maximum value
