5407058308f200e8e859227cf56be5c5f93f2a60
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-10-16 11:46:51 +0600

    solve max problem

---
+++ E_Max.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int max = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int num;
+        scanf("%d", &num);
+        if (num > max)
+        {
+            max = num;
+        }
+    }
+    printf("%d", max);
+}
