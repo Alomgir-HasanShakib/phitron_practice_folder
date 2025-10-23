af1b17b0fa4d91718c7733ae9a8a7e8eff433bc3
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-10-23 14:19:26 +0600

    practicing array insert and delete in static array

---
+++ string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    // char a[4];
+
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     scanf("%c", &a[i]);
+    // };
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     printf("%c", a[i]);
+    // }
+
+    int n;
+    scanf("%d", &n);
+
+    printf("%d", n);
+    return 0;
+}
