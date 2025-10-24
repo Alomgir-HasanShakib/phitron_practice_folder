32a0bfab1bfccd4d25265b9cae2e1626728cc1ab
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-10-24 13:18:57 +0600

    practice string copy and string concatination

---
+++ module_11/string_copy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a[101], b[101];
+    scanf("%s %s", &a, &b);
+    int length = strlen(a);
+
+    for (int i = 0; i <= length; i++)
+    {
+        b[i] = a[i];
+    }
+
+    printf("%s %s", a, b);
+
+    return 0;
+}
